92ef80e5d55dbe315d35baf1623b136af4f9cc32
hoan1015 <hoan1015@gmail.com>
2022-02-26 20:18:17 +0900

    lombok설치, 오라클 19, JDBC 테스트, 히카리 라이브러리,log4j, log4j2 (sql문을 실행한 로그 직관적으로볼수있음)

---
+++ shopping/src/main/java/com/shopping/config/RootConfig.java
@@ -0,0 +1,10 @@
+package com.shopping.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages= {"com.shopping.sample"})
+public class RootConfig {
+
+}
